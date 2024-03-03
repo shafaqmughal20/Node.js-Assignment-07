b942a9568c5d4a9eeaaed5d596a30c3ba8487a73
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:24:09 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+//5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = ("Prophet Muhammad (P.B.U.H)");
+let message = ("“Strive always to excel in virtue and truth.”");
+console.log(`${famous_person} said , ${message}`);
